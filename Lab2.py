a2e8f51c69fc8b6617ed86d2faad45985be68ba3
Chua Tze Yee <tzeyee.22@ichat.sp.edu.sg>
2023-05-10 12:31:20 +0800

    lab2 that works

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,4 @@
+import statistics
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
@@ -12,31 +13,27 @@ def display_main_menu():
     return 0
 def get_user_input():
     x = input("Enter: ")
+    print(x)
     input_list = x.split(",")
+    print(input_list)
     num_list = list(map(float, input_list))
-    num_list = input_list
     print(num_list)
     return num_list
 def cal_average_temperature(num_list):
     average = sum(num_list)/len(num_list)
-    print("Average temperature = " + str(average))
+    print("Average Temperature: " + str(average))
     return average
 def calc_min_max_temperature(num_list):
     max_value = max(num_list)
-    print("Maximum number is " + max_value)
+    print("Maximum number: " + str(max_value))
     min_value = min(num_list)
-    print("Minimum number is " + min_value)
+    print("Minimum number: " + str(min_value))
 
-def cal_median_temperature(input_list):
-    sorted_list = sorted(input_list)
-    n = len(sorted_list)
-    middle = n // 2
-    if n % 2 == 0:
-        median = (sorted_list[middle - 1] + sorted_list[middle]) / 2
-    else:
-        median = sorted_list[middle]
-    print("Median temperature = " + median)
+def cal_median_temperature(num_list):
+    median = statistics.median(num_list)
+    print("Median Temperature: " + str(median))
     return median
 
-if __name__ == "__main__":
+if __name__== "__main__":
     main()
+
